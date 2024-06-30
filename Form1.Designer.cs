e5368acc5eb471d8b5055f44135ec6f839d480af
jim9292bb <jim9292bb@gmail.com>
2024-06-30 21:03:00 +0800

    更新

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.txtTime = new System.Windows.Forms.TextBox();
             this.StartAlarmButton = new System.Windows.Forms.Button();
             this.StopAlarmButton = new System.Windows.Forms.Button();
-            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.HourComboBox1 = new System.Windows.Forms.ComboBox();
             this.MinuteComboBox1 = new System.Windows.Forms.ComboBox();
             this.MinuteComboBox4 = new System.Windows.Forms.ComboBox();
@@ -47,10 +47,12 @@
             // 
             // txtTime
             // 
+            this.txtTime.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTime.Location = new System.Drawing.Point(253, 26);
             this.txtTime.Name = "txtTime";
-            this.txtTime.Size = new System.Drawing.Size(284, 22);
+            this.txtTime.Size = new System.Drawing.Size(284, 33);
             this.txtTime.TabIndex = 0;
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // StartAlarmButton
             // 
@@ -58,8 +60,9 @@
             this.StartAlarmButton.Name = "StartAlarmButton";
             this.StartAlarmButton.Size = new System.Drawing.Size(205, 39);
             this.StartAlarmButton.TabIndex = 1;
-            this.StartAlarmButton.Text = "button1";
+            this.StartAlarmButton.Text = "啟動鬧鐘";
             this.StartAlarmButton.UseVisualStyleBackColor = true;
+            this.StartAlarmButton.Click += new System.EventHandler(this.StartAlarmButton_Click);
             // 
             // StopAlarmButton
             // 
@@ -67,92 +70,103 @@
             this.StopAlarmButton.Name = "StopAlarmButton";
             this.StopAlarmButton.Size = new System.Drawing.Size(205, 39);
             this.StopAlarmButton.TabIndex = 2;
-            this.StopAlarmButton.Text = "button2";
+            this.StopAlarmButton.Text = "關閉鬧鐘";
             this.StopAlarmButton.UseVisualStyleBackColor = true;
+            this.StopAlarmButton.Click += new System.EventHandler(this.StopAlarmButton_Click);
             // 
-            // timer
+            // timer1
             // 
-            this.timer.Enabled = true;
-            this.timer.Interval = 1000;
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
             // 
             // HourComboBox1
             // 
+            this.HourComboBox1.Font = new System.Drawing.Font("新細明體", 16F);
             this.HourComboBox1.FormattingEnabled = true;
             this.HourComboBox1.Location = new System.Drawing.Point(253, 79);
             this.HourComboBox1.Name = "HourComboBox1";
-            this.HourComboBox1.Size = new System.Drawing.Size(121, 20);
+            this.HourComboBox1.Size = new System.Drawing.Size(121, 29);
             this.HourComboBox1.TabIndex = 3;
             // 
             // MinuteComboBox1
             // 
+            this.MinuteComboBox1.Font = new System.Drawing.Font("新細明體", 16F);
             this.MinuteComboBox1.FormattingEnabled = true;
             this.MinuteComboBox1.Location = new System.Drawing.Point(416, 79);
             this.MinuteComboBox1.Name = "MinuteComboBox1";
-            this.MinuteComboBox1.Size = new System.Drawing.Size(121, 20);
+            this.MinuteComboBox1.Size = new System.Drawing.Size(121, 29);
             this.MinuteComboBox1.TabIndex = 4;
             // 
             // MinuteComboBox4
             // 
+            this.MinuteComboBox4.Font = new System.Drawing.Font("新細明體", 16F);
             this.MinuteComboBox4.FormattingEnabled = true;
             this.MinuteComboBox4.Location = new System.Drawing.Point(416, 232);
             this.MinuteComboBox4.Name = "MinuteComboBox4";
-            this.MinuteComboBox4.Size = new System.Drawing.Size(121, 20);
+            this.MinuteComboBox4.Size = new System.Drawing.Size(121, 29);
             this.MinuteComboBox4.TabIndex = 6;
             // 
             // HourComboBox4
             // 
+            this.HourComboBox4.Font = new System.Drawing.Font("新細明體", 16F);
             this.HourComboBox4.FormattingEnabled = true;
             this.HourComboBox4.Location = new System.Drawing.Point(253, 232);
             this.HourComboBox4.Name = "HourComboBox4";
-            this.HourComboBox4.Size = new System.Drawing.Size(121, 20);
+            this.HourComboBox4.Size = new System.Drawing.Size(121, 29);
             this.HourComboBox4.TabIndex = 5;
             // 
             // MinuteComboBox5
             // 
+            this.MinuteComboBox5.Font = new System.Drawing.Font("新細明體", 16F);
             this.MinuteComboBox5.FormattingEnabled = true;
             this.MinuteComboBox5.Location = new System.Drawing.Point(416, 287);
             this.MinuteComboBox5.Name = "MinuteComboBox5";
-            this.MinuteComboBox5.Size = new System.Drawing.Size(121, 20);
+            this.MinuteComboBox5.Size = new System.Drawing.Size(121, 29);
             this.MinuteComboBox5.TabIndex = 8;
             // 
             // HourComboBox5
             // 
+            this.HourComboBox5.Font = new System.Drawing.Font("新細明體", 16F);
             this.HourComboBox5.FormattingEnabled = true;
             this.HourComboBox5.Location = new System.Drawing.Point(253, 287);
             this.HourComboBox5.Name = "HourComboBox5";
-            this.HourComboBox5.Size = new System.Drawing.Size(121, 20);
+            this.HourComboBox5.Size = new System.Drawing.Size(121, 29);
             this.HourComboBox5.TabIndex = 7;
             // 
             // MinuteComboBox3
             // 
+            this.MinuteComboBox3.Font = new System.Drawing.Font("新細明體", 16F);
             this.MinuteComboBox3.FormattingEnabled = true;
             this.MinuteComboBox3.Location = new System.Drawing.Point(416, 180);
             this.MinuteComboBox3.Name = "MinuteComboBox3";
-            this.MinuteComboBox3.Size = new System.Drawing.Size(121, 20);
+            this.MinuteComboBox3.Size = new System.Drawing.Size(121, 29);
             this.MinuteComboBox3.TabIndex = 10;
             // 
             // HourComboBox3
             // 
+            this.HourComboBox3.Font = new System.Drawing.Font("新細明體", 16F);
             this.HourComboBox3.FormattingEnabled = true;
             this.HourComboBox3.Location = new System.Drawing.Point(253, 180);
             this.HourComboBox3.Name = "HourComboBox3";
-            this.HourComboBox3.Size = new System.Drawing.Size(121, 20);
+            this.HourComboBox3.Size = new System.Drawing.Size(121, 29);
             this.HourComboBox3.TabIndex = 9;
             // 
             // MinuteComboBox2
             // 
+            this.MinuteComboBox2.Font = new System.Drawing.Font("新細明體", 16F);
             this.MinuteComboBox2.FormattingEnabled = true;
             this.MinuteComboBox2.Location = new System.Drawing.Point(416, 130);
             this.MinuteComboBox2.Name = "MinuteComboBox2";
-            this.MinuteComboBox2.Size = new System.Drawing.Size(121, 20);
+            this.MinuteComboBox2.Size = new System.Drawing.Size(121, 29);
             this.MinuteComboBox2.TabIndex = 12;
             // 
             // HourComboBox2
             // 
+            this.HourComboBox2.Font = new System.Drawing.Font("新細明體", 16F);
             this.HourComboBox2.FormattingEnabled = true;
             this.HourComboBox2.Location = new System.Drawing.Point(253, 131);
             this.HourComboBox2.Name = "HourComboBox2";
-            this.HourComboBox2.Size = new System.Drawing.Size(121, 20);
+            this.HourComboBox2.Size = new System.Drawing.Size(121, 29);
             this.HourComboBox2.TabIndex = 11;
             // 
             // Form1
@@ -185,7 +199,7 @@
         private System.Windows.Forms.TextBox txtTime;
         private System.Windows.Forms.Button StartAlarmButton;
         private System.Windows.Forms.Button StopAlarmButton;
-        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ComboBox HourComboBox1;
         private System.Windows.Forms.ComboBox MinuteComboBox1;
         private System.Windows.Forms.ComboBox MinuteComboBox4;
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,118 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace SimpleClock
 {
     public partial class Form1 : Form
     {
+        private Timer timer; // 用於更新時間顯示和檢查鬧鐘的計時器
+        private WaveOutEvent waveOut; // 用於播放音效的 WaveOut 物件
+        private AudioFileReader audioFileReader; // 用於讀取音效文件的 AudioFileReader 物件
+
         public Form1()
         {
             InitializeComponent();
+            InitializeComboBoxes(); // 初始化小時和分鐘的 ComboBox
+            InitializeTimer(); // 初始化計時器
+            InitializeNAudio(); // 初始化 NAudio 用於播放鬧鐘音效
+        }
+
+        // 初始化小時和分鐘的 ComboBox
+        private void InitializeComboBoxes()
+        {
+            // 設置小時 ComboBox
+            for (int i = 0; i < 24; i++)
+            {
+                HourComboBox1.Items.Add(i.ToString("D2"));
+                HourComboBox2.Items.Add(i.ToString("D2"));
+                HourComboBox3.Items.Add(i.ToString("D2"));
+                HourComboBox4.Items.Add(i.ToString("D2"));
+                HourComboBox5.Items.Add(i.ToString("D2"));
+            }
+
+            // 設置分鐘 ComboBox
+            for (int i = 0; i < 60; i++)
+            {
+                MinuteComboBox1.Items.Add(i.ToString("D2"));
+                MinuteComboBox2.Items.Add(i.ToString("D2"));
+                MinuteComboBox3.Items.Add(i.ToString("D2"));
+                MinuteComboBox4.Items.Add(i.ToString("D2"));
+                MinuteComboBox5.Items.Add(i.ToString("D2"));
+            }
+
+            // 設置預設選項
+            HourComboBox1.SelectedIndex = 0;
+            HourComboBox2.SelectedIndex = 0;
+            HourComboBox3.SelectedIndex = 0;
+            HourComboBox4.SelectedIndex = 0;
+            HourComboBox5.SelectedIndex = 0;
+
+            MinuteComboBox1.SelectedIndex = 0;
+            MinuteComboBox2.SelectedIndex = 0;
+            MinuteComboBox3.SelectedIndex = 0;
+            MinuteComboBox4.SelectedIndex = 0;
+            MinuteComboBox5.SelectedIndex = 0;
+        }
+
+        // 初始化計時器
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; // 設置計時器間隔為1秒
+            timer.Tick += Timer_Tick; // 註冊計時器的 Tick 事件處理函數
+            timer.Start(); // 啟動計時器
+        }
+
+        // 初始化 NAudio
+        private void InitializeNAudio()
+        {
+            waveOut = new WaveOutEvent(); // 初始化 WaveOut 物件
+            audioFileReader = new AudioFileReader("alarm_sound.mp3"); // 初始化 AudioFileReader 物件並加載音效文件
+            waveOut.Init(audioFileReader); // 初始化 WaveOut 物件以播放音效文件
+        }
+
+        // 計時器的 Tick 事件處理函數
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now; // 獲取當前時間
+            txtTime.Text = now.ToString("HH:mm:ss"); // 更新顯示當前時間的 TextBox
+
+            CheckAlarms(now); // 檢查是否有鬧鐘時間到
+        }
+
+        // 檢查鬧鐘時間
+        private void CheckAlarms(DateTime now)
+        {
+            // 收集所有設定的鬧鐘時間
+            string[] alarmTimes = new string[]
+            {
+                $"{HourComboBox1.SelectedItem}:{MinuteComboBox1.SelectedItem}",
+                $"{HourComboBox2.SelectedItem}:{MinuteComboBox2.SelectedItem}",
+                $"{HourComboBox3.SelectedItem}:{MinuteComboBox3.SelectedItem}",
+                $"{HourComboBox4.SelectedItem}:{MinuteComboBox4.SelectedItem}",
+                $"{HourComboBox5.SelectedItem}:{MinuteComboBox5.SelectedItem}"
+            };
+
+            // 如果當前時間與任一鬧鐘時間匹配，則播放音效
+            if (alarmTimes.Contains(now.ToString("HH:mm")))
+            {
+                waveOut.Play();
+            }
+        }
+
+        // 開始鬧鐘的按鈕點擊事件處理函數
+        private void StartAlarmButton_Click(object sender, EventArgs e)
+        {
+            timer.Start(); // 開始計時器
+        }
+
+        // 停止鬧鐘的按鈕點擊事件處理函數
+        private void StopAlarmButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop(); // 停止計時器
+            waveOut.Stop(); // 停止播放音效
         }
     }
 }
